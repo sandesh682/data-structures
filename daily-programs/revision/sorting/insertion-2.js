c623125d3ab2510af66cf2346039e6ffcb2c15ac
Sandesh Nangare <sandesh682@gmail.com>
2025-10-21 07:23:51 +0530

    insertion sort is revised

---
+++ daily-programs/revision/sorting/insertion-2.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && current < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(insertionSort([5, 4, 3, 2, 1]));
